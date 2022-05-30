430d9452193df139e042bf8fcdbef15b85635592
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-05-30 18:55:40 +0900

    Add source

---
+++ src/SandboxConfiguration.ts
@@ -0,0 +1,53 @@
+/**
+ * akashic-sandbox, akashic-cli-serve の設定表すインターフェース。
+ */
+export interface SandboxConfiguration {
+	/**
+	 * @deprecated 非推奨。将来削除する予定。
+	 * autoSendEventName の使用が正となる。autoSendEventName が存在する場合にこの値は無視される。
+	 */
+	autoSendEvents?: string;
+	/**
+	 * コンテンツ起動時にイベントを自動送信するイベント名。
+	 */
+	autoSendEventName?: string;
+	/**
+	 * 画像のローカルパス、もしくは URL を指定することでコンテンツ実行画面の背景に画像を表示する。
+	 */
+	backgroundImage?: string;
+	/**
+	 * コンテンツ実行画面の背景色。
+	 */
+	backgroundColor?: string;
+	/**
+	 * ページ読み込み時にデベロッパーメニューを表示するか否か。
+	 * 省略された場合、偽。
+	 */
+	showMenu?: boolean;
+	/**
+	 * コンテンツで扱う playlog イベント。
+	 */
+	events?: { [name: string]: any };
+	/**
+	 * コンテンツの起動時に渡される引数。
+	 */
+	arguments?: { [name: string]: any };
+	/**
+	 * 外部アセットの値。
+	 * serve で外部アセットを利用する場合は `--allow-external` オプションを利用。
+	 */
+	externalAssets?: (string | RegExp)[] | null;
+	/**
+	 * 将来の拡張に備えたバージョン識別子。
+	 * 現状この値は "1" もしくは省略されなければなりません。
+	 */
+	formatVersion?: "1";
+	/**
+	 * serve の client で利用できる外部プラグインを登録。
+	 */
+	client?: {
+		external?: {
+			[key: string]: string; // 値は各プラグインの scriptPath
+		};
+	};
+}
---
+++ src/utils/utils.ts
@@ -0,0 +1,32 @@
+import * as path from "path";
+import type { SandboxConfiguration } from "../SandboxConfiguration";
+
+/**
+ * g.Game#external に渡されるオブジェクトを返す
+ */
+export function getServeExternalValue(externalScriptPath: any): () => any {
+	let gameExternalFactory: () => any = () => undefined;
+	try {
+		gameExternalFactory = require(path.resolve(externalScriptPath));
+	} catch (e) {
+		throw new Error(`Failed to evaluating externalScript (${externalScriptPath}): ${e}`);
+	}
+	if (typeof gameExternalFactory !== "function") {
+		throw new Error(`${externalScriptPath}, given as externalScript, does not export a function`);
+	}
+
+	return gameExternalFactory;
+}
+
+/**
+ * 正規化
+ */
+export function normalize(sandboxConfig: SandboxConfiguration): void {
+	const { events, autoSendEvents, autoSendEventName } = sandboxConfig;
+
+	if (!autoSendEventName && events && autoSendEvents && events[autoSendEvents] instanceof Array) {
+		// TODO: `autoSendEvents` は非推奨。互換性のためこのパスを残しているが、`autoSendEvents` の削除時にこのパスも削除する。
+		console.warn("[deprecated] `autoSendEvents` in sandbox.config.js is deprecated. Please use `autoSendEventName`.");
+		sandboxConfig.autoSendEventName = autoSendEvents;
+	}
+}
